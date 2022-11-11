a6734de7b9aca328b98a0c8a012db8d94cb1be6c
henrichslucas <84094692+henrichslucas@users.noreply.github.com>
2022-11-11 10:33:27 -0300

    Aula 40 | Enviando eventos do child pro parent, exemplo 2

---
+++ communication-app/src/app/clients/clients.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css'],
+})
+export class ClientsComponent implements OnInit {
+  constructor() {}
+  name: string = '';
+
+  ngOnInit(): void {}
+}
